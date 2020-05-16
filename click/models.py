a31cedd9b7473b06a836deb7ddbba54c1c0e41e1
thaonguyen0229 <47158178+thaonguyen0229@users.noreply.github.com>
2020-05-16 17:07:16 -0400

    Listener.join() is called in index.html

--- click/models.py
+++ click/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from pynput.mouse import Listener
 
-def on_click(x, y, button, pressed):
-    if pressed and button == button.left:
-        print ('Left clicked at ({0}, {1})'.format(x, y))
+class Click(models.Model):
 
-with Listener(on_click=on_click) as listnr:
-    listnr.join()
+    def listener_join():
+        def on_click(x, y, button, pressed):    
+            if pressed and button == button.left:
+                print ('Left clicked at ({0}, {1})'.format(x, y))
+        with Listener(on_click=on_click) as listnr:
+            listnr.join()
