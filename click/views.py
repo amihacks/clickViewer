23c20cb023daedeb467ef405469bdf7841167b71
Harry Pino-Rosado <48214389+isaacp99@users.noreply.github.com>
2020-05-16 01:11:08 -0400

    Add CSS stylesheet, HTML index page, and updated views
    Stylesheet is empty, index is a work in progress.
    TODO: Get the functionality running and then implement into views, and fix other connections

--- click/views.py
+++ click/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Placeholder text for the click index.")
+    return render(request, 'click/index.html')
