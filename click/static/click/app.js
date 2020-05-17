9139547a2c89e4bc29eda751ebbe13a813fa80d1
Tmddhks014 <michael.lim014@gmail.com>
2020-05-17 05:05:10 -0400

    added mouse drawing
    added drawing pixels in js code
    onclicked in html canvas

--- click/static/click/app.js
+++ click/static/click/app.js
@@ -3,3 +3,14 @@ var canvas = document.getElementById("C");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#000000";
 ctx.fillRect(0, 0, canvas.clientHeight, canvas.clientWidth);
+
+function drawSquare(x,y,color) {
+	ctx.fillStyle = color;
+	ctx.fillRect(x, y, 10, 10);
+}
+
+function clicked(){
+    var x = event.clientX;
+    var y = event.clientY;
+    drawSquare(x, y, #FFFFFF)
+}
